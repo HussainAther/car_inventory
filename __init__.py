0b704d698b2e2bc5bcd86094a51a06ddb68b0e00
Syed Hussain Ather <shussainather@gmail.com>
2023-06-26 18:06:25 -0400

    blueprint3 login and register info

--- __init__.py
+++ __init__.py
@@ -3,7 +3,13 @@ import logging # For error handling
 from flask import Flask
 from .blueprint1.routes import blueprint1
 from .blueprint2.routes import blueprint2
+from .blueprint3.routes import blueprint3
+
+from .blueprint3.models import User
 from .config import config
+from .models import db
+
+from flask_login import current_user
 from .routes import main_blueprint
 
 def create_app():
@@ -20,8 +26,22 @@ def create_app():
 
     app.register_blueprint(blueprint1, url_prefix='/blueprint1')
     app.register_blueprint(blueprint2, url_prefix='/blueprint2')
+    app.register_blueprint(blueprint3, url_prefix='/blueprint3')
 
     logging.info("Blueprints registered successfully")
+
+    # Initialize database
+    db.init_app(app)
+
+    # Load the user database
+    @app.before_first_request
+    def load_user_database():
+        with app.app_context():
+            db.create_all()
+            if not User.query.filter_by(username='admin').first():
+                admin = User(username='admin')
+                admin.set_password('password')
+                admin.save()
 
     return app
 
