c603f47faff3e332aad31f808bd41bb2f55de890
Syed Hussain Ather <shussainather@gmail.com>
2023-06-26 17:54:44 -0400

    register the apps

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
+import logging # For error handling
+
 from flask import Flask
 from .blueprint1.routes import blueprint1
 from .blueprint2.routes import blueprint2
 from .config import config
+from .routes import main_blueprint
 
 def create_app():
     app = Flask(__name__)
@@ -10,9 +13,15 @@ def create_app():
     # Enable debug mode
     app.debug = True
     
-    # Register blueprints
+    # # Register blueprints
+    # app.register_blueprint(blueprint1)
+    # app.register_blueprint(blueprint2)
+    app.register_blueprint(main_blueprint)
+
     app.register_blueprint(blueprint1, url_prefix='/blueprint1')
     app.register_blueprint(blueprint2, url_prefix='/blueprint2')
+
+    logging.info("Blueprints registered successfully")
 
     return app
 
