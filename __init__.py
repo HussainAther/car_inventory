283a828a93baa16a6cd8ac16dbea44f6221f1ba0
Syed Hussain Ather <shussainather@gmail.com>
2023-06-26 15:04:59 -0400

    app instructions

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from .blueprint1.routes import blueprint1
 from .blueprint2.routes import blueprint2
+from .config import config
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
+    
+    # Register blueprints
     app.register_blueprint(blueprint1, url_prefix='/blueprint1')
     app.register_blueprint(blueprint2, url_prefix='/blueprint2')
 
