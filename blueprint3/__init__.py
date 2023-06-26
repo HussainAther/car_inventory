e3dbf8048ea7958f710db444d4d92befa1ba5fc9
Syed Hussain Ather <shussainather@gmail.com>
2023-06-26 18:15:10 -0400

    login and register info

--- blueprint3/__init__.py
+++ blueprint3/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_login import LoginManager
 
-blueprint3 = Blueprint('blueprint3', __name__)
+blueprint3 = Blueprint('blueprint3', __name__, template_folder='blueprint3_folder')
 
 # Create a LoginManager instance
 login_manager = LoginManager()
@@ -9,3 +9,11 @@ login_manager = LoginManager()
 # Configure Flask-Login
 login_manager.init_app(blueprint3)
 login_manager.login_view = 'blueprint3.login'
+
+@blueprint3.route('/login')
+def index():
+    return render_template('login.html')
+
+@blueprint3.route('/register')
+def index():
+    return render_template('register.html')
